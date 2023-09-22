95bb1afc1bd3715ec7c270990f2a61742ff06e13
rshaker101 <136416532+rshaker101@users.noreply.github.com>
2023-09-21 18:10:34 -0700

    Add files via upload
    updated files

--- module-6-challenge/api_keys.py
+++ module-6-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ba39d789747343a52d08560edaaf78df"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "690d09233d804eef922163a1455829a3"
